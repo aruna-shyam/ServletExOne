9e9cc9570fc40de731c828a6c2c8112f86144d9a
aruna-shyam <arunarsha1412@gmail.com>
2023-03-11 16:35:13 +0530

    Uploading Project

---
+++ src/com/techpalle/MyServlet.java
@@ -0,0 +1,29 @@
+package com.techpalle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//Without url servlet cannot display the o/p 1st aproach
+//@WebServlet("/My")
+public class MyServlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		//it is used to display some content 
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		//calling doGet method
+		doGet(request, response);
+	}
+
+}
